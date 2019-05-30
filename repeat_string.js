55e450876660c7f564b2d9631507376cbf318afa
Skepsis101 <kevinpatriciosp@gmail.com>
2019-05-30 14:19:43 -0300

    Finished the repeat string exercise

---
+++ repeat_string.js
@@ -0,0 +1,9 @@
+var stringRepeat = "repeating";
+var counter = 1;
+    function repeatString(){
+        while (counter < 4){
+            console.log(stringRepeat, counter + " times");
+            counter++
+        }
+    }
+repeatString(); //calling the function
